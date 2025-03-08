d29d32abb011cdfca2956a7d924a7f191695794e
himanshu <himanshukaushiik@gmail.com>
2025-03-08 15:02:45 +0530

    new commit added in this form

--- reverse.c
+++ reverse.c
@@ -3,9 +3,15 @@
 
 int main(){
 
-    char str[]="hello himanshu";
-    int n = strlen(str);
+    char str[100];
+    printf("enter the string = ");
 
+
+    fgets(str , sizeof(str) , stdin);
+
+
+    int n = strlen(str);
+    
     for (int  i = n-1; i >= 0; i--)
     {
         printf("%c \n", str[i]);
